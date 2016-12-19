58c3295f1577a03379a6e4ac8f6244bdfb787d3f
[wanwang2] <632468635@qq.com>
2016-12-19 10:12:45 +0800

    v

---
+++ wanwan-blog2017/src/main/java/org/wanwanframework/blog/App.java
@@ -0,0 +1,13 @@
+package org.wanwanframework.blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
